01637aabd7999529970163ed81daed195f8a576a
Vladimir Poliakov <60501887+bobruk76@users.noreply.github.com>
2020-04-07 23:43:59 +0300

    end of day

--- users.py
+++ users.py
@@ -11,28 +11,6 @@ DB_PATH = "sqlite:///sochi_athletes.sqlite3"
 # базовый класс моделей таблиц
 Base = declarative_base()
 
-class Athelete(Base):
-    """
-    Описывает структуру таблицы athelete для хранения регистрационных данных пользователей
-    """
-    # задаем название таблицы
-    __tablename__ = 'athelete'
-
-    # поля таблицы
-    id = sa.Column(sa.Integer, primary_key=True)
-    age = sa.Column(sa.Integer)
-    birthdate = sa.Column(sa.Text)
-    gender = sa.Column(sa.Text)
-    height = sa.Column(sa.REAL)
-    name = sa.Column(sa.Text)
-    weight = sa.Column(sa.Integer)
-    gold_medals = sa.Column(sa.Integer)
-    silver_medals = sa.Column(sa.Integer)
-    bronze_medals = sa.Column(sa.Integer)
-    total_medals  = sa.Column(sa.Integer)
-    sport  = sa.Column(sa.Text)
-    country  = sa.Column(sa.Text)
-
 class User(Base):
     """
     Описывает структуру таблицы user для хранения регистрационных данных пользователей
@@ -41,7 +19,7 @@ class User(Base):
     __tablename__ = 'user'
 
     # поля таблицы
-    id = sa.Column(sa.String(36), primary_key=True)
+    id = sa.Column(sa.Integer, primary_key=True)
     first_name = sa.Column(sa.Text)
     last_name = sa.Column(sa.Text)
     gender = sa.Column(sa.Text)
@@ -49,32 +27,64 @@ class User(Base):
     birthdate = sa.Column(sa.Text)
     height = sa.Column(sa.REAL)
 
-def find_gender(gender, session):
-    query = session.query(Athelete).filter(Athelete.gender == gender)
-
-    return [item.name for item in query.all()]
-
-def find_age(age, session):
-    query = session.query(Athelete).filter(Athelete.age > age)
-
-    return [item.name for item in query.all()]
-
-def find_gender_age_gold_medal(gender, age, session):
-    query = session.query(Athelete).filter(Athelete.gender == gender).filter(Athelete.age > age).filter(Athelete.gold_medals > 1)
-
-    return [item.name for item in query.all()]
 def connect_db():
     engine = sa.create_engine(DB_PATH)
     Base.metadata.create_all(engine)
     session = sessionmaker(engine)
     return session()
 
+def valid_email(email):
+    email_list = [item.split('.') for item in email.split('@')]
+    return len(email_list) == 2 and len(email_list[-1]) > 1
+
+gender_set = ('Male', 'Female', 'Other')
+def valid_gender(gender):
+    return gender in gender_set
+
+def request_data():
+
+    """
+    Запрашивает у пользователя данные и добавляет их в список users
+    """
+    # выводим приветствие
+    print("Введите данные пользователя:")
+    # запрашиваем у пользователя данные
+    first_name = str(input("Имя пользователя: "))
+    last_name = str(input("Фамилию: "))
+
+    gender =''
+    while not valid_gender(gender):
+        gender = str(input("Пол:"))
+        if not valid_gender(gender):
+            print('Попробуйте ввести что-то из следующего списка {}'.format(gender_set))
+
+    email =''
+    while not valid_email(email):
+        email = input("Адрес электронной почты: ")
+        if not valid_email(email):
+            print('Неправильный формат электронной почты!')
+
+    user_id = str(uuid.uuid4())
+    # создаем нового пользователя
+    user = User(
+        id=user_id,
+        first_name=first_name,
+        last_name=last_name,
+        gender = gender,
+        email = email,
+        birthdate = birthdate,
+        height = height
+    )
+    # возвращаем созданного пользователя
+    return user
+
 def main():
     """
     Осуществляет взаимодействие с пользователем, обрабатывает пользовательский ввод
     """
     session = connect_db()
-    print(len(find_gender_age_gold_medal('Male',25,  session)))
+    print(request_data())
+
 
 
 
