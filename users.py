966d65aa3b9da6808f10bd1d6da8ef27de6d138f
Vladimir Poliakov <60501887+bobruk76@users.noreply.github.com>
2020-04-07 22:54:59 +0300

    begin work

--- users.py
+++ users.py
@@ -1,8 +1,3 @@
-#CREATE TABLE athelete("id" integer primary key autoincrement, "age" integer,"birthdate" text,"gender" text,"height" real,"name" text,"weight" integer,"gold_medals" integer,"silver_medals" integer,"bronze_medals" integer,"total_medals" integer,"sport" text,"country" text);
-#CREATE TABLE sqlite_sequence(name,seq);
-#CREATE TABLE user("id" integer primary key autoincrement, "first_name" text, "last_name" text, "gender" text, "email" text, "birthdate" text, "height" real);
-
-# импортируем модули стандартной библиотеки uuid и datetime
 import uuid
 import datetime
 
@@ -12,6 +7,76 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # константа, указывающая способ соединения с базой данных
-DB_PATH = "sqlite:///users.sqlite3"
+DB_PATH = "sqlite:///sochi_athletes.sqlite3"
 # базовый класс моделей таблиц
 Base = declarative_base()
+
+class Athelete(Base):
+    """
+    Описывает структуру таблицы athelete для хранения регистрационных данных пользователей
+    """
+    # задаем название таблицы
+    __tablename__ = 'athelete'
+
+    # поля таблицы
+    id = sa.Column(sa.Integer, primary_key=True)
+    age = sa.Column(sa.Integer)
+    birthdate = sa.Column(sa.Text)
+    gender = sa.Column(sa.Text)
+    height = sa.Column(sa.REAL)
+    name = sa.Column(sa.Text)
+    weight = sa.Column(sa.Integer)
+    gold_medals = sa.Column(sa.Integer)
+    silver_medals = sa.Column(sa.Integer)
+    bronze_medals = sa.Column(sa.Integer)
+    total_medals  = sa.Column(sa.Integer)
+    sport  = sa.Column(sa.Text)
+    country  = sa.Column(sa.Text)
+
+class User(Base):
+    """
+    Описывает структуру таблицы user для хранения регистрационных данных пользователей
+    """
+    # задаем название таблицы
+    __tablename__ = 'user'
+
+    # поля таблицы
+    id = sa.Column(sa.String(36), primary_key=True)
+    first_name = sa.Column(sa.Text)
+    last_name = sa.Column(sa.Text)
+    gender = sa.Column(sa.Text)
+    email = sa.Column(sa.Text)
+    birthdate = sa.Column(sa.Text)
+    height = sa.Column(sa.REAL)
+
+def find_gender(gender, session):
+    query = session.query(Athelete).filter(Athelete.gender == gender)
+
+    return [item.name for item in query.all()]
+
+def find_age(age, session):
+    query = session.query(Athelete).filter(Athelete.age > age)
+
+    return [item.name for item in query.all()]
+
+def find_gender_age_gold_medal(gender, age, session):
+    query = session.query(Athelete).filter(Athelete.gender == gender).filter(Athelete.age > age).filter(Athelete.gold_medals > 1)
+
+    return [item.name for item in query.all()]
+def connect_db():
+    engine = sa.create_engine(DB_PATH)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(engine)
+    return session()
+
+def main():
+    """
+    Осуществляет взаимодействие с пользователем, обрабатывает пользовательский ввод
+    """
+    session = connect_db()
+    print(len(find_gender_age_gold_medal('Male',25,  session)))
+
+
+
+if __name__ == "__main__":
+    main()
